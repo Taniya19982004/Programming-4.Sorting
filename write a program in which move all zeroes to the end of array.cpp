9f311ad1a308be466d6e225d428222c5840e5dd1
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-02-06 11:39:10 +0530

    Add files via upload

---
+++ write a program in which move all zeroes to the end of array.cpp
@@ -0,0 +1,53 @@
+//User function template for C++
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    vector<int>temp;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]!=0)
+	        {
+	            temp.push_back(arr[i]);
+	        }
+	    }
+	    
+	    int s=temp.size();
+        
+	    for(int i=0;i<n;i++)
+	    {
+	        if(i<s)
+	        {
+	            arr[i]=temp[i];
+	        }else{
+	            arr[i]=0;
+	        }
+	    }
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+	}
+};
+
+int main()
+{
+    int arr[6]={2,0,3,4,0,0};
+    Solution obj;
+    obj.pushZerosToEnd(arr,6);
+
+}
+/*  for(int i=0;i<n;i++)
+	    {
+	        for(int j=i+1;j<n;j++)
+	        {
+	          if(arr[i]==0 &&arr[j]!=0)
+	        {
+	            swap(arr[i],arr[j]);
+	        }
+
+	        }
+	     
+	    }
+	}*/
