d7781c49d36f358cd06ee77585503df9f7ace111
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-02-08 20:13:11 +0530

    Add files via upload

---
+++ Check if array is rotated and sorted.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(vector<int>&v)
+{
+    int count = 0;
+    int n=v.size();
+    for(int i=1;i<n;i++)
+    {
+        if(v[i-1]>v[i])
+        {
+           count++;
+        }
+    }
+    if(v[n-1]>v[0])
+    {
+        count++;
+    }
+    if(count<=1)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
+
+int main(){
+    vector<int> v={3,4,5,1,2};
+    bool ans=check(v);
+    cout<<(ans?"true":"false");
+    
+
+return 0;
+}
